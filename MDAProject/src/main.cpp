d20d29b511fe328cc5785a79500ab40d46b5da2c
Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>
2016-04-14 11:18:16 -0500

    conclue this repo
    Signed-off-by: Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>

--- MDAProject/src/main.cpp
+++ MDAProject/src/main.cpp
@@ -3,6 +3,7 @@
 #include "ModelDrivenArch.hpp"
 #include "AbstractFactory.hpp"
 #include "Accounts.hpp"
+#include "Actions.hpp"
 
 
 using namespace std;
@@ -16,6 +17,22 @@ void testAccount1() {
         Account1* a1 = new Account1(mda);
         a1->init(cf1);
 
+        a1->open("123", "A1", 450.0);
+        //a1->login("A1");
+        //a1->pin("321");
+        //a1->pin("123");
+        //a1->balance();
+        //a1->deposit(60.0);
+        //a1->balance();
+        //a1->withdraw(200.0);
+        //a1->lock("321");
+        //a1->lock("123");
+        //a1->balance();
+        //a1->unlock("123");
+        //a1->deposit(600.0);
+        //a1->balance();
+        //a1->logout();
+
         delete op1;
         delete a1;
 }
@@ -23,11 +40,11 @@ void testAccount1() {
 void testAccount2() {
         ConcreteFactory2* cf2 = new ConcreteFactory2();
         OutputProcessor* op2 = new OutputProcessor();
-        op1->init(cf2);
+        op2->init(cf2);
         
         ModelDrivenArch* mda = new ModelDrivenArch(op2);
-        Account1* a2 = new Account2(mda);
-        a1->init(cf2);
+        Account2* a2 = new Account2(mda);
+        a2->init(cf2);
 
         delete op2;
         delete a2;
